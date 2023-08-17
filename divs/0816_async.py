63748e993b0fb476e706c1a8cfda62210b86524b
Weihuang415 <50312976+Weihuang415@users.noreply.github.com>
2023-08-17 12:09:07 -0700

    Update 0816_async.py

--- divs/0816_async.py
+++ divs/0816_async.py
@@ -20,46 +20,51 @@ YOUTUBE_API_VERSION = 'v3'
 pickedVid = ''
 cachedVids = sorted(glob.glob('VideoDL/*.mp4'))
 print("cachedVids:", cachedVids)
-output_directory = 'VideoDL'  
-
+output_dir = 'VideoDL'  
 
 Played = True
 playing = False
 screenUp = False
 canFlip =False
-SearchResult = []
-i = 0
+
+searchResult = []
+i_search = 0
 
 def youtube_search():
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-
-    search_response = youtube.search().list(
-        q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),
-        part='snippet',
-        type="video",
-        videoDefinition="standard",
-        videoDimension="2d",
-        videoDuration="short",
-        maxResults=10
-    ).execute()
-
     videos = []
-
-    for search_result in search_response.get('items', []):
-        videos.append(search_result['id']['videoId'])
-        #print(videos)
+    
+    while len(videos) < 5:
+        try:
+            search_response = youtube.search().list(
+                q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),
+                part='snippet',
+                type="video",
+                videoDefinition="standard",
+                videoDimension="2d",
+                videoDuration="short",
+                maxResults=5
+            ).execute()
+            
+            for search_result in search_response.get('items', []):
+                videos.append(search_result['id']['videoId'])
+        
+        except Exception as e:
+            print(e)        
+    print('videos:', videos)
     return videos
 
 
 ### DOWNLOAD
 async def downloadVid():
-    global SearchResult
-    print(SearchResult)
-    video_id = SearchResult[i] #youtube_search()
+    global searchResult
+    print(searchResult)
+    
+    video_id = searchResult[i_search] #youtube_search()
     if video_id:
         url = "https://www.youtube.com/watch?v=" + video_id
         print(url)
-        print("i", i)
+        print("i", i_search)
 
         ydl_opts = {
             'format': 'mp4[height<720]',
@@ -67,7 +72,7 @@ async def downloadVid():
             'nocheckcertificate': True,
             'progress_hooks': [my_hook],
             # 'outtmpl': + str(int(time.time()))[-4:] + '_'+ video_id + '.mp4'
-            'outtmpl': os.path.join(output_directory, 'yt_' + str(int(time.time()))[-4:] + '_'+ video_id + '.mp4')
+            'outtmpl': os.path.join(output_dir, 'yt_' + str(int(time.time()))[-4:] + '_'+ str(video_id)[4:] + '.mp4')
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
@@ -121,10 +126,10 @@ def basic_auth(username, password):
 async def playNext():
     global playing
     global played    
+    global pickedVid
     
     if cachedVids:
         playing = True
-        
         #Playthe First one
         pickedVid = cachedVids[0] 
         play_url = f'http://127.0.0.1:8080/requests/status.json'
@@ -146,12 +151,24 @@ async def playNext():
 #Stop the Video
 def stopPlaying():
     global played
+    global playing
+    global pickedVid
+    
     if playing:
-        defaultVid = 'VideoDefault\Default.mp4'
+        
         play_url = f'http://127.0.0.1:8080/requests/status.json'
         headers = {'Authorization': basic_auth("", "asdf")}
         
+        #Stop Video
+        params = {'command': 'pl_stop', 'input': os.path.abspath(pickedVid)}
+        response = requests.get(play_url, headers=headers, params=params)
+        if response.status_code == 200:
+            print(f'VLC stoped the picked video: {pickedVid}')
+        else:
+            print(f'Failed to stop the picked video. Status code: {response.status_code}')
+            
         # Play default video
+        defaultVid = 'VideoDefault\Default2.mp4'    
         params = {'command': 'in_play', 'input': os.path.abspath(defaultVid)}
         response = requests.get(play_url, headers=headers, params=params)
         if response.status_code == 200:
@@ -160,25 +177,22 @@ def stopPlaying():
             print(f'Failed to play the default video. Status code: {response.status_code}')
 
         sleep(1)  # delay between default video and picked video
-    
-        #Stop Video
-        params = {'command': 'pl_stop', 'input': os.path.abspath(pickedVid)}
-        response = requests.get(play_url, headers=headers, params=params)
-        if response.status_code == 200:
-            print(f'VLC stoped the picked video: {pickedVid}')
-        else:
-            print(f'Failed to stop the picked video. Status code: {response.status_code}')
 
         played = True
+        
     else:
         print("No videos playing.")
 
 def delete_video():
     if played:
-        print(f"{cachedVids[0]} Removing the played video.")
+        print(f"{cachedVids[0]} --- Removing the played video.")
         try:
-            os.remove(cachedVids[0])
-            print(f"Existing video {cachedVids[0]} removed.")
+            os.remove(cachedVids[0])  #remove the file in VideoDL folder
+            print(f"{cachedVids[0]} --- Removed.")
+            
+            cachedVids.pop(0)   #remove from the list
+            print(f"{cachedVids[0]} --- first video in cachedVids.")
+            
         except OSError as e:
             print(f"Error removing video {cachedVids[0]}: {e}")
 
@@ -190,41 +204,52 @@ async def Tasks():
 if __name__ == '__main__':
         
     VLC_open()
-    
-    SearchResult = youtube_search()
-    print('SearchResult:', SearchResult)
-    #print('First Video:', SearchResult[0]) 
-    
+
+    searchResult = youtube_search()
+    print('SearchResult:', searchResult)    
     
     while True:
-        
         key = input("Press 'N' to play or 'X' to exit: ")
-        
+    
         if key == 'n': #flip up
             
             #Async 
             asyncio.run(Tasks())
             
-            i += 1
-            print("index:", i)
+            i_search += 1
+            print("index:", i_search)
             
             #search again if it runs out of list
-            if i >= 10:
+            if i_search >= 5:
                 SearchResult = youtube_search() 
-                i = 0
+                i_search = 0
                 
         elif key == 'd': #flip down
             stopPlaying()
-            sleep(0.5)
-            
             delete_video()
-            
-            #######????######
-            pickedVid = cachedVids[1]
-            print("###pickedVid", pickedVid) 
                        
         else:
             break
         
         
+'''
+if __name__ == '__main__':
+    playVideo()
+    downloadOnly()
+    while True:
+        #sensorDetect()
 
+        if screenUp == False:  # It's down
+            canFlip = True
+            print('/////Down/////')   
+             
+        else:                  # It's up
+            print('up')
+            if canFlip == True:
+                playNext()
+                downloadOnly()
+                canFlip = False # Set it to False after playing the video
+                
+        sleep(0.8)
+        
+'''
